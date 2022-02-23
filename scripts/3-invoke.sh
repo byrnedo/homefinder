bc0d6417fb88067716a92288c9ef8b6755e8f2ee
Donal Byrne <byrnedo@tcd.ie>
2022-02-23 15:44:43 +0100

    Fix invoke

--- scripts/3-invoke.sh
+++ scripts/3-invoke.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 FUNCTION=$(aws cloudformation describe-stack-resource --region us-east-1 --stack-name homefinder --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text)
 echo $FUNCTION
 while true; do
-  aws lambda invoke --function-name $FUNCTION --region us-east-1 --payload file://event.json out.json
+  aws lambda invoke --function-name $FUNCTION --region us-east-1 --payload "$(base64 ./event.json)" out.json
   cat out.json
   echo ""
   sleep 2
